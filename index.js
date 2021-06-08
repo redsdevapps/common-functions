62537a1b8337add088d832b73ad53c64c902bba7
Víctor Hugo Rojas López <victorhugo.rojas.lopez@bbva.com>
2021-06-08 13:05:12 -0500

    Correct npm distribution files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import * as _Commons from './src/CommonFunctions';
-import * as _ObjectUtils from './src/ObjectUtils';
-import * as _StringUtils from './src/StringUtils';
-import * as _Calendar from './src/Calendar';
+import * as _Commons from './dis/CommonFunctions';
+import * as _ObjectUtils from './dis/ObjectUtils';
+import * as _StringUtils from './dis/StringUtils';
+import * as _Calendar from './dis/Calendar';
 
 export const Commons = _Commons;
 export const StringUtils = _StringUtils;
