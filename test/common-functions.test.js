6106b24354e751144a3d7f4d486c94cc41144c99
Víctor Hugo Rojas López <victorhugo.rojas.lopez@bbva.com>
2024-01-25 09:19:32 -0600

    fix: remove unused dependencies

--- test/common-functions.test.js
+++
@@ -1,9 +0,0 @@
-import { expect } from '@open-wc/testing';
-
-import { ObjectUtils } from '../index';
-
-describe('ObjectUtils', () => {
-  it('Not null', () => {
-    expect(ObjectUtils.isObject).to.not.be.null;
-  });
-});
